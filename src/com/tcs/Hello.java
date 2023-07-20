f0dee3028257824868394186bef192c3d3897ddf
suryacs113 <suryacs113@gmail.com>
2023-07-20 12:57:22 +0530

    This is Ravi Branch

--- src/com/tcs/Hello.java
+++ src/com/tcs/Hello.java
@@ -1,11 +1,20 @@
 package com.tcs;
 
 public class Hello {
+	
+	
 
 	public static void main(String[] args) {
 
-		
 		System.out.println("Hello");
+		
 	}
+	
+	public void text() {
+		System.out.println("This is Ravi Branch");
+		
+	}
+	
+	
 
 }
